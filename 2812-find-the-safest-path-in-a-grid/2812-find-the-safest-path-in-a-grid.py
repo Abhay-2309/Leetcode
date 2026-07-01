80f0fea325ee092564a562f67ed41b34cf763ddc
Abhay Sharma <abhayrsharma1000@gmail.com>
2026-07-01 14:23:31 +0530

    Time: 2811 ms (79.19%), Space: 31.3 MB (68.21%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.py
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.py
@@ -1,72 +1,41 @@
+import heapq
 from collections import deque
 from typing import List
 
 class Solution:
-    def __init__(self):
-        self.directions = [(1, 0), (-1, 0), (0, -1), (0, 1)]
-
-    def check(self, distNearestThief: List[List[int]], sf: int, n: int) -> bool:
-        que = deque()
-        visited = [[False] * n for _ in range(n)]
-        que.append((0, 0))
-        visited[0][0] = True
-
-        if distNearestThief[0][0] < sf:
-            return False
-
-        while que:
-            curr_i, curr_j = que.popleft()
-
-            if curr_i == n - 1 and curr_j == n - 1:
-                return True
-
-            for dr, dc in self.directions:
-                new_i = curr_i + dr
-                new_j = curr_j + dc
-
-                if 0 <= new_i < n and 0 <= new_j < n and not visited[new_i][new_j]:
-                    if distNearestThief[new_i][new_j] < sf:
-                        continue
-                    
-                    que.append((new_i, new_j))
-                    visited[new_i][new_j] = True
-
-        return False
-
     def maximumSafenessFactor(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        distNearestThief = [[-1] * n for _ in range(n)]
-        que = deque()
-        visited = [[False] * n for _ in range(n)]
+        m = len(grid[0])
+        vis = [[float('inf')] * m for _ in range(n)]
+        q = deque()
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        
         for i in range(n):
-            for j in range(n):
+            for j in range(m):
                 if grid[i][j] == 1:
-                    que.append((i, j))
-                    visited[i][j] = True
-
-        level = 0
-        while que:
-            size = len(que)
-            for _ in range(size):
-                curr_i, curr_j = que.popleft()
-                distNearestThief[curr_i][curr_j] = level
-                for dr, dc in self.directions:
-                    new_i = curr_i + dr
-                    new_j = curr_j + dc
-                    if new_i < 0 or new_i >= n or new_j < 0 or new_j >= n or visited[new_i][new_j]:
-                        continue
-                    que.append((new_i, new_j))
-                    visited[new_i][new_j] = True
-
-            level += 1
-        l = 0
-        r = 400
-        result = 0
-        while l <= r:
-            mid_sf = l + (r - l) // 2
-            if self.check(distNearestThief, mid_sf, n):
-                result = mid_sf
-                l = mid_sf + 1
-            else:
-                r = mid_sf - 1
-        return result
+                    vis[i][j] = 0
+                    q.append((0, i, j))
+                    
+        while q:
+            efforts, r, c = q.popleft()
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < n and 0 <= nc < m:
+                    if vis[nr][nc] > 1 + vis[r][c]:
+                        vis[nr][nc] = 1 + vis[r][c]
+                        q.append((vis[nr][nc], nr, nc))
+        pq = [(-vis[0][0], 0, 0)]
+        seen = [[False] * m for _ in range(n)]
+        seen[0][0] = True
+        while pq:
+            safe, r, c = heapq.heappop(pq)
+            safe = -safe
+            if r == n - 1 and c == m - 1:
+                return safe
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < n and 0 <= nc < m and not seen[nr][nc]:
+                    seen[nr][nc] = True
+                    new_safe = min(safe, vis[nr][nc])
+                    heapq.heappush(pq, (-new_safe, nr, nc))
+        return 0
