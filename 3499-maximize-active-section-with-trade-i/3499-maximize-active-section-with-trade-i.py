f13cc1b35421f75f4c797d3c3cc0e54eb5b69298
Abhay Sharma <abhayrsharma1000@gmail.com>
2026-07-21 15:35:59 +0530

    Time: 1685 ms (5.61%), Space: 28 MB (7.48%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.py
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.py
@@ -3,32 +3,43 @@ class Solution:
         n = len(s)
         one = s.count("1")
         s = "1" + s + "1"
-        newlength = len(s)
-        leftmefirstone = [-1] * (newlength + 1)
-        rightmefirstone = [newlength] * (newlength + 1)
-        for i in range(0, newlength):
+        newlength = n + 2
+        leftmefirstone = [0] * newlength
+        rightmefirstone = [0] * newlength
+        last_one = -1
+        for i, char in enumerate(s):
+            if char == "1":
+                last_one = i
+            leftmefirstone[i] = last_one
+                
+        last_one = newlength
+        for i in range(newlength - 1, -1, -1):
             if s[i] == "1":
-                leftmefirstone[i] = i
-            else:
-                leftmefirstone[i] = leftmefirstone[i-1]
-        for i in range(newlength-1, -1, -1):
-            if s[i] == "1":
-                rightmefirstone[i] = i
-            else:
-                rightmefirstone[i] = rightmefirstone[i+1]
+                last_one = i
+            rightmefirstone[i] = last_one
                 
         maxi = one
         ptr = 1
-        while ptr < newlength - 1:
+        limit = newlength - 1
+        
+        while ptr < limit:
             if s[ptr] == "1":
                 left = ptr
                 right = ptr
+                
                 while right + 1 < newlength and s[right + 1] == "1":
                     right += 1
+                
                 if s[left - 1] == "0" and right + 1 < newlength and s[right + 1] == "0":
                     nextfirstone = rightmefirstone[right + 2]
                     prevfirstone = leftmefirstone[left - 1]
-                    maxi = max(maxi, min(n, one + nextfirstone - prevfirstone - 1 - (right-left+1)))
+                    current_val = one + nextfirstone - prevfirstone - right + left - 2
+                    
+                    if current_val > n:
+                        current_val = n
+                    if current_val > maxi:
+                        maxi = current_val
+                
                 ptr = right + 1
             else:
                 ptr += 1
