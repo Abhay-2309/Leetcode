bb27c3c94c6684fb53d7289672a4a2f5427b5706
Abhay Sharma <abhayrsharma1000@gmail.com>
2026-07-21 21:31:51 +0530

    Time: 352 ms (41.87%), Space: 171.6 MB (90.28%) - LeetHub

--- 3999-minimum-number-of-string-groups-through-transformations/3999-minimum-number-of-string-groups-through-transformations.cpp
+++ 3999-minimum-number-of-string-groups-through-transformations/3999-minimum-number-of-string-groups-through-transformations.cpp
@@ -2,21 +2,15 @@ class Solution {
 public:
     static const int base = 31;
     static const int MOD = 1000000087;
-
+    
     static inline int modadd(int a, int b) {
-        int res = a + b;
-        return res >= MOD ? res - MOD : res;
-    }
-
-    static inline int modsub(int a, int b) {
-        int res = a - b;
-        return res < 0 ? res + MOD : res;
-    }
-
+        return (a + b) % MOD;
+    } 
+    
     static inline int modmull(int a, int b) {
         return (1LL * a * b) % MOD;
     }
-
+    
     static inline int modPow(int b, int exp) {
         int res = 1;
         b %= MOD;
@@ -27,9 +21,9 @@ public:
         }
         return res;
     }
-
+    
     inline static const vector<int> powerof31 = []() {
-        int MAX_LEN = 500005;
+        int MAX_LEN = 500005; 
         vector<int> p(MAX_LEN);
         p[0] = 1;
         for (int i = 1; i < MAX_LEN; i++) {
@@ -42,91 +36,74 @@ public:
         int MAX_LEN = 500005;
         vector<int> inv(MAX_LEN);
         inv[0] = 1;
-        int inv_base = modPow(base, MOD - 2);
-
+        int inv_base = modPow(base, MOD - 2); 
+        
         for (int i = 1; i < MAX_LEN; i++) {
             inv[i] = modmull(inv[i - 1], inv_base);
         }
         return inv;
     }();
-
+    
     int minimumGroups(vector<string>& words) {
         int n = (int)words.size();
-
-        unordered_set<uint64_t> st;
-        st.reserve(n);
-
-        for (const auto& str : words) {
+        set<pair<int, int>> st;
+        
+        for(auto str: words){
             string odd = "", even = "";
-            even.reserve(str.size());
-            odd.reserve(str.size());
-
             int ptr = 0;
-            while (ptr < str.size()) {
-                if (ptr & 1)
-                    odd += str[ptr];
-                else
-                    even += str[ptr];
+            while(ptr < str.size()){
+                if(ptr & 1) odd += (str[ptr]);
+                else even += (str[ptr]);
                 ptr++;
             }
-
+            
             int size1 = (int)even.size();
             int size2 = (int)odd.size();
             even += even;
             odd += odd;
-
+            
             int evenhash = 0;
             int oddhash = 0;
-
-            for (int i = 0; i < size1; i++) {
-                evenhash = modadd(evenhash, modmull((even[i] - 'a' + 1), powerof31[i]));
+            
+            for(int i = 0; i < size1; i++){
+                evenhash = modadd(evenhash, modmull((even[i]-'a' + 1), powerof31[i]));
             }
-
-            for (int i = 0; i < size2; i++) {
-                oddhash = modadd(oddhash, modmull((odd[i] - 'a' + 1), powerof31[i]));
+            for(int i = 0; i < size2; i++){
+                oddhash = modadd(oddhash, modmull((odd[i]-'a' + 1), powerof31[i]));
             }
-
+            
             int minieven = evenhash;
             int miniodd = oddhash;
-
             if (size1 > 0) {
                 int left = 1;
                 int right = size1;
-
-                while (right < even.size()) {
-                    int remove_val = (even[left - 1] - 'a' + 1);
-
-                    evenhash = modsub(evenhash, remove_val);
+                while(right < even.size()){
+                    int remove_val = (even[left-1] - 'a' + 1);
+                    evenhash = (evenhash - remove_val + MOD) % MOD;
                     evenhash = modmull(evenhash, inversemodulo[1]);
-                    evenhash = modadd(evenhash, modmull((even[right] - 'a' + 1), powerof31[size1 - 1]));
-
+                    evenhash = modadd(evenhash, modmull((even[right]-'a' + 1), powerof31[size1-1]));
+                    
                     minieven = min(minieven, evenhash);
                     left++;
                     right++;
                 }
             }
-
             if (size2 > 0) {
                 int left = 1;
                 int right = size2;
-
-                while (right < odd.size()) {
-                    int remove_val = (odd[left - 1] - 'a' + 1);
-
-                    oddhash = modsub(oddhash, remove_val);
+                while(right < odd.size()){
+                    int remove_val = (odd[left-1] - 'a' + 1);
+                    oddhash = (oddhash - remove_val + MOD) % MOD;
                     oddhash = modmull(oddhash, inversemodulo[1]);
-                    oddhash = modadd(oddhash, modmull((odd[right] - 'a' + 1), powerof31[size2 - 1]));
-
+                    oddhash = modadd(oddhash, modmull((odd[right]-'a' + 1), powerof31[size2-1]));
+                    
                     miniodd = min(miniodd, oddhash);
                     left++;
                     right++;
                 }
             }
-
-            uint64_t key = ((uint64_t)minieven << 32) | (uint32_t)miniodd;
-            st.insert(key);
+            st.insert({minieven, miniodd});
         }
-
         return st.size();
     }
 };
