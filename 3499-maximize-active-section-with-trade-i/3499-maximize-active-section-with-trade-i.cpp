90924570673408ab5eb01a94f84e71807e12bfd3
Abhay Sharma <abhayrsharma1000@gmail.com>
2026-07-21 15:39:37 +0530

    Time: 224 ms (6.3%), Space: 164.6 MB (13.06%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -2,25 +2,48 @@ class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
         int n = s.length();
-        int activeCount = count(begin(s), end(s), '1');
-        vector<int> inactiveBlocks;
-        int i = 0;
-        while(i < n) {
-            if(s[i] == '0') {
-                int start = i;
-                while(i < n && s[i] == '0') i++;
-
-                inactiveBlocks.push_back(i-start);
+        int one = count(s.begin(), s.end(), '1');
+        s = "1" + s + "1";
+        int newlength = s.length();
+        
+        vector<int> leftmefirstone(newlength + 1, -1);
+        vector<int> rightmefirstone(newlength + 1, newlength);
+        for (int i = 0; i < newlength; ++i) {
+            if (s[i] == '1') {
+                leftmefirstone[i] = i;
             } else {
-                i++;
+                leftmefirstone[i] = leftmefirstone[i - 1];
             }
         }
-
-        int maxPairSum = 0;
-        for(int i = 1; i < inactiveBlocks.size(); i++) {
-            maxPairSum = max(maxPairSum, inactiveBlocks[i] + inactiveBlocks[i-1]);
+        for (int i = newlength - 1; i >= 0; --i) {
+            if (s[i] == '1') {
+                rightmefirstone[i] = i;
+            } else {
+                rightmefirstone[i] = rightmefirstone[i + 1];
+            }
         }
-
-        return maxPairSum + activeCount;
+        
+        int maxi = one;
+        int ptr = 1;
+        while (ptr < newlength - 1) {
+            if (s[ptr] == '1') {
+                int left = ptr;
+                int right = ptr;
+                while (right + 1 < newlength && s[right + 1] == '1') {
+                    right++;
+                }
+                if (s[left - 1] == '0' && right + 1 < newlength && s[right + 1] == '0') {
+                    int nextfirstone = rightmefirstone[right + 2];
+                    int prevfirstone = leftmefirstone[left - 1];
+                    int current_val = one + nextfirstone - prevfirstone - 1 - (right - left + 1);
+                    maxi = max(maxi, min(n, current_val));
+                }
+                ptr = right + 1;
+            } else {
+                ptr++;
+            }
+        }
+        
+        return maxi;
     }
 };
