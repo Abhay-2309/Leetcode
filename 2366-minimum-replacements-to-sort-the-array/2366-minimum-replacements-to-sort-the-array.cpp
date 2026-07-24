7a64fc3b14be6c9d2f2cda189a479a1b62088438
Abhay Sharma <abhayrsharma1000@gmail.com>
2026-07-25 00:35:18 +0530

    Time: 0 ms (100%), Space: 58.5 MB (36.97%) - LeetHub

---
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long minimumReplacement(vector<int>& nums) {
+        int n = nums.size();
+        long long cost =0;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<=nums[i+1]) continue;
+            int parts = nums[i]/nums[i+1];
+            if(nums[i]%nums[i+1]!=0) parts++;
+            cost += (parts-1);
+            nums[i] = nums[i]/parts;
+         }
+
+        return cost;
+
+    }
+};
