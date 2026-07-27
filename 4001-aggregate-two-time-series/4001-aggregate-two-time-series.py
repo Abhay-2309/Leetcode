59cd7dccc59b4d44ccbdc941265c7853b6ccc87f
Abhay Sharma <abhayrsharma1000@gmail.com>
2026-07-27 09:01:44 +0530

    Time: 89 ms (100%), Space: 67.9 MB (100%) - LeetHub

--- 4001-aggregate-two-time-series/4001-aggregate-two-time-series.py
+++ 4001-aggregate-two-time-series/4001-aggregate-two-time-series.py
@@ -1,42 +1,30 @@
 class Solution:
     def aggregateTimeSeries(self, series1: list[list[int]], series2: list[list[int]]) -> list[list[int]]:
-        n = len(series1)
-        m = len(series2)
+        p1 = len(series1) - 1
+        p2 = len(series2) - 1
+        next_v1 = 0
+        next_v2 = 0
         
-        first_val = defaultdict(int)
-        second_val = defaultdict(int)
-        totalid = set()
-        
-        for i in range(n):
-            first_val[series1[i][0]] = series1[i][1]
-            totalid.add(series1[i][0])
+        res = []
+        while p1 >= 0 or p2 >= 0:
+            t1 = series1[p1][0] if p1 >= 0 else -1
+            t2 = series2[p2][0] if p2 >= 0 else -1
             
-        for i in range(m):
-            second_val[series2[i][0]] = series2[i][1]
-            totalid.add(series2[i][0])
-            
-        t1 = [x[0] for x in series1]
-        t2 = [x[0] for x in series2]
-        
-        for ids in totalid:
-            if ids in first_val and ids in second_val:
-                continue
-            if ids in first_val and ids not in second_val:
-                index = bisect_left(t2, ids)
-                if index != m:
-                    second_val[ids] = series2[index][1]
-                else:
-                    second_val[ids] = 0
-                continue
-            if ids in second_val and ids not in first_val:
-                index = bisect_left(t1, ids)
-                if index != n:
-                    first_val[ids] = series1[index][1]
-                else:
-                    first_val[ids] = 0
-                    
-        result = []
-        for ids in sorted(totalid):
-            result.append([ids, first_val[ids] + second_val[ids]])
-            
-        return result
+            if t1 > t2:
+                next_v1 = series1[p1][1]
+                res.append([t1, next_v1 + next_v2])
+                p1 -= 1
+                
+            elif t2 > t1:
+                next_v2 = series2[p2][1]
+                res.append([t2, next_v1 + next_v2])
+                p2 -= 1
+                
+            else:
+                next_v1 = series1[p1][1]
+                next_v2 = series2[p2][1]
+                res.append([t1, next_v1 + next_v2])
+                p1 -= 1
+                p2 -= 1
+        res.reverse()
+        return res
