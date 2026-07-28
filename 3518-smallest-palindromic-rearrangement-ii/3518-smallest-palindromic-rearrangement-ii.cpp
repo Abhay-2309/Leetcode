1382b8ec3fc75fa48685cea9df3053f7f703ff88
Abhay Sharma <abhayrsharma1000@gmail.com>
2026-07-28 13:07:36 +0530

    Time: 17 ms (69.12%), Space: 17.5 MB (33.82%) - LeetHub

--- 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.cpp
+++ 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.cpp
@@ -1,64 +1,80 @@
-#define ll long long
 class Solution {
 public:
-    const ll CAP = 1000000;
-    ll binomCap(ll n, ll r){
-        if(r < 0 || r > n) return 0;
-        r = min(r, n - r);
-        ll res = 1;
-        for(ll i = 1; i <= r; i++){
-            res = res * (n - r + i) / i;  
-            if(res > CAP) return CAP;
+    const int MOD = 1e9 + 7;
+    int modpow(long long base, int exp) {
+        long long res = 1;
+        base %= MOD;
+        while (exp > 0) {
+            if (exp % 2 == 1) {
+                res = (res * base) % MOD;
+            }
+            base = (base * base) % MOD;
+            exp /= 2;
         }
         return res;
     }
-    ll perms(vector<int>& cnt){
-        ll res = 1, rem = 0;
-        for(int c = 0; c < 26; c++){
-            if(!cnt[c]) continue;
-            rem += cnt[c];
-            res = res * binomCap(rem, cnt[c]);
-            if(res > CAP) return CAP;
-        }
-        return res;
+    int modmul(int a, int b) {
+        return (1LL * a * b) % MOD;
+    }
+    int moddiv(int a, int b) {
+        return modmul(a, modpow(b, MOD - 2));
     }
 
     string smallestPalindrome(string s, int k) {
-        int n = s.size();
-        int m = n >> 1;
-        string half = s.substr(0, m);
-        sort(half.begin(), half.end());
-        string mid = (n & 1) ? string(1, s[m]) : "";
+        k--;
+        int n = (s.length()>>1);
+        string str(s.begin(), s.begin() + n);
 
-        vector<int> cnt(26, 0);
-        for(char ch : half) cnt[ch - 'a']++;
+        sort(str.begin(), str.end());
 
-        if(perms(cnt) < k) return ""; 
-        vector<int> suf(26, 0);
-        int t = 0;
-        while(t < m){
-            suf[half[m - 1 - t] - 'a']++;
-            t++;
-            if(perms(suf) >= k) break; 
-        }
-
-        string ans = half.substr(0, m - t); 
-        ll kk = k;
-        for(int pos = 0; pos < t; pos++){
-            for(int c = 0; c < 26; c++){
-                if(!suf[c]) continue;
-                suf[c]--;
-                ll ways = perms(suf);      
-                if(ways >= kk){             
-                    ans += char('a' + c);
-                    break;
-                }
-                kk -= ways;        
-                suf[c]++;
+        vector<short> freq(500, 0);
+        int fact = 1;
+        int denom = 1;
+        int sp = -1;
+        int cnt;
+        if(k==0) goto here;
+        for(int i = n-1, j = 1; i>=0; i--, j++){
+            denom = modmul(denom, ++freq[str[i]]);
+            fact = modmul(fact, j);
+            int res = moddiv(fact, denom);
+            if(res > k){
+                sp = i;
+                cnt = j;
+                break;
             }
         }
-        string rev = ans;
-        reverse(rev.begin(), rev.end());
-        return ans + mid + rev;
+
+        if(sp==-1) return "";
+
+        while(k){
+            fact = moddiv(fact, cnt--);
+            for(char i = 'a'; i<='z'; i++){
+                if(!freq[i]) continue;
+                denom = moddiv(denom, freq[i]);
+                int res = moddiv(fact, denom);
+                if(res <= k){
+                    k -= res;
+                    denom = modmul(denom, freq[i]);
+                }
+                else{
+                    freq[i]--;
+                    str[sp] = i;
+                    break;
+                }
+            }
+            sp++;
+
+        }
+
+        for(char i = 'a'; i<='z'; i++){
+            while(freq[i]--) str[sp++] = i;
+        }
+        here:
+        string fStr = str;
+        if(s.length()&1) fStr += s[n];
+        reverse(str.begin(), str.end());
+        fStr += str;
+        return fStr;
+
     }
 };
