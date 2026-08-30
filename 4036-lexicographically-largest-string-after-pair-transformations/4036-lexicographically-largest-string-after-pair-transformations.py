ea97f1b833075ddae9ceaa7c9bc76caa511918a1
Abhay Sharma <abhayrsharma1000@gmail.com>
2026-08-30 07:46:17 +0530

    Time: 911 ms (75%), Space: 41.1 MB (100%) - LeetHub

--- 4036-lexicographically-largest-string-after-pair-transformations/4036-lexicographically-largest-string-after-pair-transformations.py
+++ 4036-lexicographically-largest-string-after-pair-transformations/4036-lexicographically-largest-string-after-pair-transformations.py
@@ -1,19 +1,12 @@
 class Solution:
     def largestString(self, nums: list[int]) -> list[str]:
-        n = len(nums)
-        numbertochar = {
-            1:'a', 2:'b', 3:'c', 4:'d', 5:'e', 6:'f', 7:'g', 8:'h', 9:'i',
-            10:'j', 11:'k', 12:'l', 13:'m', 14:'n', 15:'o', 16:'p', 17:'q',
-            18:'r', 19:'s', 20:'t', 21:'u', 22:'v', 23:'w', 24:'x', 25:'y', 26:'z'
-        }
         ans = []
         for num in nums:
-            res = ""
-            for bit in range(0,27):
-                if (num>>bit)&1:
-                    if(bit!=26):
-                        res += numbertochar[bit+1]
-                    else:
-                        res+='zz'
-            ans.append(res[::-1])
+            res = []
+            if (num >> 26) & 1:
+                res.append("zz")
+            for bit in range(25, -1, -1):
+                if (num >> bit) & 1:
+                    res.append(chr(ord('a') + bit))
+            ans.append("".join(res))
         return ans
